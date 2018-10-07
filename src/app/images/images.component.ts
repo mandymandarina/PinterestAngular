63c3ad3d261b543ea186445b863125e2aab62db1
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-10-07 20:23:09 -0300

    muestra tags

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -10,13 +10,21 @@ import { ImageService } from '../shared/image.service';
 export class ImagesComponent implements OnInit {
 
 images: any[];
-
+imagesFound: boolean = false;
+   handleSuccess(data){
+    this.imagesFound = true;
+    this.images = data.hits;
+    console.log(data.hits);
+  }
+   handleError(error){
+    console.log(error);
+  }
   constructor(private _imageService : ImageService) { }
 
   searchImages(query: string){
     return this._imageService.getImage(query).subscribe(
-      data => console.log(data),
-      error => console.log(error),
+      data => this.handleSuccess(data),
+      error => this.handleError(error),
       () => console.log("Request complete!")
     )
   }
